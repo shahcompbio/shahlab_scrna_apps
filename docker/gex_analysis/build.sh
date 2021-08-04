546ae3d389418d791cf2be9bc729254e6bed047b
Jason Azayev <azayevjason@gmail.com>
2021-08-04 10:20:37 -0400

    Move docker and remove push

--- docker/gex_analysis/build.sh
+++ docker/gex_analysis/build.sh
@@ -8,6 +8,4 @@ echo "VERSION: ${VERSION}"
 
 echo "\n BUILDING \n"
 
-docker build -t $REGISTRY/$ORG/gex_analysis:$VERSION . --no-cache
-
-docker push $REGISTRY/$ORG/gex_analysis:$VERSION
+docker build -t $REGISTRY/$ORG/gex_analysis:$VERSION ../../apps/gex_analysis --no-cache
