a612ff3acce24237fff42d105af82bafdd7a968f
Jason Azayev <azayevjason@gmail.com>
2021-08-05 15:07:49 -0400

    run docker

--- docker/gex_analysis/test.sh
+++ docker/gex_analysis/test.sh
@@ -7,4 +7,4 @@ VERSION=$3
 
 echo "\n PREPARING TO TEST \n"
 
-docker exec $REGISTRY/$ORG/gex_analysis:$VERSION pytest --pyargs gex_analysis
+docker run $REGISTRY/$ORG/gex_analysis:$VERSION pytest --pyargs gex_analysis
